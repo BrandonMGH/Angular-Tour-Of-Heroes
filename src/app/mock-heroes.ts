3e8ad214c0c91a855480f0aed28b6b6c322d73c6
BrandonMGH <BrandonMGH@Yahoo.com>
2020-03-10 19:30:49 -0700

    finished the Display a List portion of the Tour of Heroes App

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', ability: "Invisibility" },
+  { id: 12, name: 'Narco', ability: "Super Speed" },
+  { id: 13, name: 'Bombasto', ability: "Super Strength" },
+  { id: 14, name: 'Celeritas', ability: "Elasticity" },
+  { id: 15, name: 'Magneta', ability: "Telekinesis" },
+  { id: 16, name: 'RubberMan', ability: "Telepathy" },
+  { id: 17, name: 'Dynama', ability: "Healer Factory" },
+  { id: 18, name: 'Dr IQ', ability: "Lightning" },
+  { id: 19, name: 'Magma', ability: "Frost" },
+  { id: 20, name: 'Tornado', ability: "Fire" }
+];
